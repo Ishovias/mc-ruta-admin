5522d0cfa8b30a545bc7b268681290d131af4739
IshoCelular <isaiasberoiza@gmail.com>
2024-04-21 18:31:13 -0400

    agregando override de manage de usuarios

--- conectorbd.py
+++ conectorbd.py
@@ -1,4 +1,5 @@
 from bd.repository import bdmediclean
+from coder.codexpy2 import codexpy2
 import params
 import os
 
@@ -46,34 +47,101 @@ class conectorbd:
          resultado = False
       return resultado
 
-   def busca_cliente(self, nombre: str) -> list:
-      filainicio = self.hojaActual["filainicio"]
-      columna = self.hojaActual["columnas"]["nombre"]
+   def busca_cliente(self, nombre: str) -> map:
+      filainicio = self.hojaActual["filainicial"]
+      columna = self.hojaActual["columnas"]["cliente"]
       filas = self.bd.buscapartedato(filainicio,columna,nombre)
-      filas.insert(0,1)
-      resultados = []
       columnas = self.hojaActual["columnas"]["todas"]
+      resultados = {}
+      resultados["encabezados"] = self.bd.extraefila(1,columnas)
+      resultados["datos"] = []
       for fila in filas:
          data = self.bd.extraefila(fila,columnas)
-         resultados.append(data)
+         resultados["datos"].append(data)
       
       return resultados
-   
-   if __name__ == '__main__':
-      os.system("clear")
-      sheetname = "autorizador"
-      bd = bdmediclean(sheetname)
-      filadisponible = bd.buscafila(1)
-      fila = filadisponible
-      datos = ["iberoiza","1234"]
-      columnainicio = 1
-      bd.ingresador(fila,datos,columnainicio)
-      
-      try:
-         bd.guardar()
-      except:
-         print("Error en guardado del libro\n\n")
-      else:
-         print("Cambios guardados\n\n")
-      finally:
-         bd.cerrar()
+
+if __name__ == '__main__':
+     def limpiapantalla(): 
+          os.system("clear")
+     def pantallamenu():
+          print("\n\n*******************\nAdministrador de usuarios\n\n")
+          print("** COMANDOS **")
+          print("adduser - 'Agrega un usuario nuevo'")
+          print("deluser - 'elimina un usuario'")
+          print("listuser - 'lista los usuarios existentes'")
+          print("----------------------------")
+     
+     def guarda(bd: object):
+          try:
+               bd.guardar()
+          except:
+               print("Error en guardado del libro\n\n")
+          else:
+               print("Cambios guardados\n\n")
+     
+     def lista_usuarios() -> None:
+          list_users = bd.listar(filainicial,[usuarios],encabezados)
+          limpiapantalla()
+          print("******** LISTA DE USUARIOS REGISTRADOS ********")
+          for usuario in list_users["datos"]:
+               print(f">>> {usuario}")
+          print("____________________\n\n\n")
+     
+     bd = bdmediclean(params.USUARIO["nombrehoja"])
+     coder = codexpy2()
+     
+     filainicial = params.USUARIO["filainicial"]
+     usuarios = params.USUARIO["columnas"]["usuario"]
+     encabezados = params.USUARIO["encabezados"]
+     
+     limpiapantalla()
+     
+     while(True):
+          
+          pantallamenu()
+          
+          comando = input("COMANDO >> ")
+          
+          if comando == "adduser":
+               usuario = input("Ingresa un nombre de usuario >> ")
+               contrasena = input("Ingresa una contrasena >> ")
+               if (usuario != "" or usuario != None or
+               contrasena != "" or contrasena != None):
+                    password = coder.encripta(contrasena)
+                    bloquevacio = bd.buscafila(filainicial)
+                    bd.ingresador(bloquevacio,[usuario,password,],1)
+                    guarda(bd)
+                    limpiapantalla()
+                    pantallamenu()
+               else:
+                    print("DEBES INDICAR UN NOMBRE DE USUARIO Y UNA CONTRASENA")
+          
+          elif comando == "deluser":
+               limpiapantalla()
+               lista_usuarios()
+               usuario = input("Usuario a eliminar >>> ")
+               if (usuario != "" or usuario != None):
+                    ubicacion = bd.buscadato(filainicial,usuarios,usuario)
+                    bd.eliminar(ubicacion)
+                    guarda(bd)
+               else:
+                    print("DEBES INDICAR UN NOMBRE DE USUARIO")
+                    
+          elif comando == "listuser":
+               lista_usuarios()
+          
+          elif comando == "quit":
+               print("\n\nBye!!\n\n")
+               break
+          
+          else:
+               print(">> COMANDO ERRONEO <<")
+          
+     bd.cerrar()
+     
+     
+     
+     
+     
+     
