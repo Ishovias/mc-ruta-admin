27eea319270f67b29cfa31b3fafda151a9dbd01b
Isho-Macbook <macbook@beroro.org>
2024-08-09 22:15:08 -0400

    Refactorizando codigo en main, agregando rutas

--- handlers/clientes.py
+++ handlers/clientes.py
@@ -1,5 +1,7 @@
 from bd.repository import bdmediclean
 from datetime import date, timedelta
+from handlers.rutas import RutaActual
+from helpers import mensajes
 import params
 
 class Clientes(bdmediclean):
@@ -59,28 +61,28 @@ class Clientes(bdmediclean):
             return True
             
     def proximo_retiro(self, rut: str, fecharetiro: str) -> str:
-         diascontrato = super().getDato(
-              fila=super().busca_ubicacion(
-                   dato=rut,
-                   columna="rut"
-                   ),
-              columna="diascontrato"
-              )
-         
-         if not diascontrato:
-              return None
-         
-         try:
-              int(diascontrato)
-         except Exception as e:
-              print(e)
-              return None
-         else:
-              lapso = timedelta(diascontrato + 2)
-         
-         fecharetiro = date.fromisoformat(fecharetiro)
-         proxretiro = date.isoformat(fecharetiro + lapso)
-         return proxretiro
+        diascontrato = super().getDato(
+            fila=super().busca_ubicacion(
+                dato=rut,
+                columna="rut"
+                ),
+            columna="diascontrato"
+            )
+        
+        if not diascontrato:
+            return None
+        
+        try:
+            int(diascontrato)
+        except Exception as e:
+            print(e)
+            return None
+        else:
+            lapso = timedelta(diascontrato + 2)
+        
+        fecharetiro = date.fromisoformat(fecharetiro)
+        proxretiro = date.isoformat(fecharetiro + lapso)
+        return proxretiro
         
     def guardar_modificacion(self, rut: str, data: list) -> bool:
         fila = super().busca_ubicacion(rut,"rut")
@@ -91,3 +93,107 @@ class Clientes(bdmediclean):
         else:
             return True
 
+def empaquetador_clientes(request: object) -> map:
+    paquete = {"pagina":"clientes.html"}
+
+    if "buscacliente" in request.form:
+        
+        resultados = ""
+        with Clientes() as clientesbd:
+            nombre = request.form.get("nombre")
+            resultados = clientesbd.busca_cliente_lista(nombre)
+        
+        paquete["listaclientes"] = resultados
+        
+    elif "nuevocliente" in request.form: 
+        paquete["pagina"] = "nuevoCliente.html"
+    
+    elif "guardanuevocliente" in request.form:
+        data = [
+            "activo",
+            request.form.get("rut"),
+            request.form.get("nombre"),
+            request.form.get("direccion"),
+            request.form.get("comuna"),
+            request.form.get("telefono"),
+            request.form.get("gps"),
+            request.form.get("otros")
+            ]
+        
+        with Clientes() as bd:
+            if bd.nuevo_cliente(data):
+                paquete["alerta"] = mensajes.CLIENTE_GUARDADO.value
+            else:
+                paquete["alerta"] = mensajes.CLIENTE_GUARDADO_ERROR.value
+    
+    elif "modificaCliente" in request.form:
+        resultados = ""
+        with Clientes() as clientesbd:
+            identificador = request.form.get("clienteSeleccion")
+            resultados = clientesbd.busca_datoscliente(identificador,"rut")
+        paquete["modificacion"] = resultados
+    
+    elif "aRuta" in request.form:
+        fecha = None
+        with RutaActual() as rutaactualbd:
+            fecha = rutaactualbd.getFechaRuta()
+        if not fecha:
+            paquete["alerta"] = "Debes crear primero la ruta"
+            paquete["nuevaruta"] = True
+            paquete["pagina"] = "rutas.html"
+        else:
+            identificador = request.form.get("aRuta")
+            
+            cliente = []
+            with Clientes() as clientesbd:
+                cliente = clientesbd.busca_datoscliente(identificador,"rut")
+                cliente.remove(cliente[0]) # olculta eliminando el indicador estado del cliente, innecesario para lista de ruta
+
+            aruta = False
+            with RutaActual() as rutaactualbd:
+                aruta = rutaactualbd.agregar_a_ruta(fecha, cliente)
+
+            if aruta:
+                paquete["alerta"] = mensajes.CLIENTE_A_RUTA.value
+            else:
+                paquete["alerta"] = mensajes.CLIENTE_EN_RUTA.value
+                
+    
+    elif "darbaja" in request.form:
+        dadobaja = False
+        guardado = False
+        
+        with Clientes() as clientesbd:
+            identificador = request.form.get("rut")
+            dadobaja = clientesbd.estado_cliente(identificador,"de baja")
+
+        if dadobaja and guardado:
+            paquete["alerta"] = mensajes.CLIENTE_BAJA.value
+        else:
+            paquete["alerta"] = mensajes.CLIENTE_BAJA_ERROR.value
+            
+    
+    elif "guardamod" in request.form:
+        rut = request.form.get("rut")
+        data = [
+            rut,
+            request.form.get("nombre"),
+            request.form.get("direccion"),
+            request.form.get("comuna"),
+            request.form.get("telefono"),
+            request.form.get("gps"),
+            request.form.get("otros")
+            ]
+        
+        guardado = False
+        grabado = False
+        
+        with Clientes() as bd:
+            guardado = bd.guardar_modificacion(rut,data)
+
+        if guardado and grabado:
+            paquete["alerta"] = mensajes.CLIENTE_GUARDADO.value
+        else:
+            paquete["alerta"] = mensajes.CLIENTE_GUARDADO_ERROR.value
+
+    return paquete
--- main.py
+++ main.py
@@ -1,39 +1,49 @@
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, redirect, request, render_template, url_for
 from coder.codexpy2 import codexpy2
-from helpers import lista_rutas, SessionSingleton, verificatoken, empaquetador, mensajes
+from handlers.clientes import empaquetador_clientes
+from handlers.rutas import empaquetador_registros_rutas, empaquetador_rutaactual
+from helpers import SessionSingleton, empaquetador_login
 
 app = Flask(__name__)
 coder = codexpy2()
 sesion = SessionSingleton()
 
-               
 @app.route('/', methods=['GET','POST'])
 def index() -> render_template:
-     
-     destino = "index"
-     
-     if request.method == "POST":
-          # print(request.form)
-          for direccion in lista_rutas:
-               for item in lista_rutas[direccion]:
-                    if item in request.form:
-                         destino = direccion
-                         break
-          if "cierrasesion" in request.form:
-               if sesion.getAutenticado(request):
-                    sesion.cierraSesion(request)
-               
-     datos = empaquetador(coder, request, destino)
-     
-     if not sesion.getAutenticado(request):
-          datos["pagina"] = "autorizador.html"
-     
+     if sesion.getAutenticado(request):
+          datos = {"pagina":"index.html"}
+     else:
+          return redirect(url_for('login'))
      return render_template(datos["pagina"], datos=datos)
 
-@app.route('/codex', methods=['GET','POST'])
-def codex() -> render_template:
-     datos = empaquetador(coder, request, "codexpy")
+@app.route('/login', methods=['GET','POST'])
+def login() -> render_template:
+     if not sesion.getAutenticado(request):
+          datos = empaquetador_login(request)
+     else:
+          return redirect(url_for('index'))
+
+@app.route('/clientes', methods=['POST'])
+def clientes() -> render_template:
+     datos = empaquetador_clientes(request)
      return render_template(datos["pagina"], datos=datos)
+
+@app.route('/rutaactual', methods=['POST'])
+def rutaactual() -> render_template:
+     datos = empaquetador_rutaactual(request)
+     return render_template(datos["pagina"], datos=datos)
+
+@app.route('/rutas', methods=['POST'])
+def rutas() -> render_template:
+     datos = empaquetador_registros_rutas(request)
+     return render_template(datos["pagina"], datos=datos)
+
+@app.route('/logout', methods=['POST'])
+def rutas() -> render_template:
+     if sesion.getAutenticado(request):
+          datos = {"alerta":f"Sesion de usuario {sesion.getUsuario(request)} cerrada"}
+          sesion.cierraSesion(request)
+     return render_template('autorizador.html', datos=datos)
 
 if __name__ == '__main__':
      app.run(debug=True,host='0.0.0.0')
