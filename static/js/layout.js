80b2e2a7e5eb016ec2934c39144f539c6f6df87e
Isho-Macbook <macbook@beroro.org>
2024-08-23 22:17:37 -0400

    Trabajando en algunos estilos

--- static/js/layout.js
+++ static/js/layout.js
@@ -1,15 +1,3 @@
 function alerta(mensaje){
     alert(mensaje)
 };
-
-// deprecated
-function calculaedad(idelemento1, idelemento2){
-  var fecha = document.getElementById(idelemento1).value;
-  
-}
-
-// deprecated
-function seleccionanombre(){
-    var seleccion = document.getElementById("listahermanos").value;
-    document.getElementById("nombre").value = seleccion
-}
