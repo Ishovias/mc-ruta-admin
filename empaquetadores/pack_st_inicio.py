608ee42409c28a512f0dfe282123c6ccb7b5059b
IshoCelular <isaiasberoiza@gmail.com>
2024-09-14 16:49:06 -0300

    Agregando plantillas y packs de app SUBLITOTE

---
+++ empaquetadores/pack_st_inicio.py
@@ -0,0 +1,18 @@
+from helpers import SessionSingleton
+import params
+
+
+def pack_st_login(request: object) -> map:
+     paquete = {}
+     sesion = SessionSingleton()
+     resultado = sesion.iniciarSesion(coder,request)
+     if resultado:
+          usuario = sesion.getUsuario(request, token=resultado)
+          paquete["bienvenida"] = f"Bienvenid@ {usuario} selecciona una accion..."
+          paquete["usuario"] = usuario
+          paquete["redirect"] = "st_index.html"
+          paquete["aut"] = resultado
+     else:
+          paquete["pagina"] = "st_autorizador.html"
+          paquete["alerta"] = "Usuario o contraseña invalida"
+     return paquete
