25171a2e125d97416fea47decb0e6c6fa66193ea
IshoCelular <isaiasberoiza@gmail.com>
2024-09-17 15:43:57 -0300

    Agregando clases de sublitote

--- handlers/sublitote.py
+++ handlers/sublitote.py
@@ -1,10 +1,14 @@
 from bd.repository import bdmediclean
+from datetime import datetime
 import params
 
 class SublitoteProductos(bdmediclean):
 
     def __init__(self) -> None:
-        super().__init__(params.ST_PRODUCTOS)
+        super().__init__(
+             hoja=params.ST_PRODUCTOS,
+             otrolibro=params.LIBROST
+             )
 
     def listar_productos(self) -> map:
         resultados = super().listar()
@@ -25,93 +29,135 @@ class SublitoteProductos(bdmediclean):
             codigo = 1000
         return codigo
 
-    def nuevo_cliente(self, retornoFila: bool=False, **data) -> bool:
-        existencia = super().busca_ubicacion(dato=data["cliente"])
+    def nuevo_producto(self, retornaFila: bool=False, retornaCodigo: bool=False, **data) -> bool:
+        existencia = super().busca_ubicacion(data["producto"],"producto")
         if existencia:
             return False
-        fila = super().busca_ubicacion(columna="rut")
+        fila = super().busca_ubicacion(columna="codigo")
+        codigo = self.nuevo_codigo()
+        data["codigo"] = codigo
         for dato in data.keys():
-            super().putDato(dato=data[dato], fila=fila, columna=str(dato))
-        if retornoFila:
+            super().putDato(
+                 dato=data[dato], 
+                 fila=fila, 
+                 columna=str(dato)
+                 )
+        if retornaFila and retornaCodigo:
+            return [fila, codigo]
+        elif retornaFila:
             return fila
+        elif retornaFila:
+            return codigo
         return True
-    
-    def verifica_existencia(self, dato: str, columna: str="rut", retornafila=False) -> bool:
-        verificacion = super().buscadato(
-            self.hoja_actual["filainicial"],
-            self.hoja_actual["columnas"][columna],
-            dato,
-            filtropuntuacion=True
-            )
-        if verificacion:
-            if retornafila:
-                return verificacion
-            return True
-        return False
-    
-    def busca_datoscliente(self, nombre: str, filtro: str="cliente") -> list:
-        ubicacion = super().buscadato(
-            self.hoja_actual["filainicial"],
-            self.hoja_actual["columnas"][filtro],
-            nombre
-            )
-        if ubicacion == 0:
-            return 0
-        datos = super().extraefila(
-            fila=ubicacion,
-            columnas=[
-                self.hoja_actual["columnas"]["rut"],
-                self.hoja_actual["columnas"]["cliente"],
-                self.hoja_actual["columnas"]["direccion"],
-                self.hoja_actual["columnas"]["comuna"],
-                self.hoja_actual["columnas"]["telefono"],
-                self.hoja_actual["columnas"]["diascontrato"],
-                self.hoja_actual["columnas"]["otro"]
-            ])
-        return datos
-    
-    def estado_cliente(self, rut: str, estado: str) -> bool:
-        ubicacion = super().busca_ubicacion(rut,"rut")
+        
+    def modifica_producto(self, codigo: str, **data) -> bool:
+        fila = super().busca_ubicacion(codigo,"codigo")
+        if not fila:
+             print("ERROR METODO MODIFICA_PRODUCTO: No existe codigo indicado")
+             return False
         try:
-            super().ingresador(
-                ubicacion,
-                [estado],
-                [self.hoja_actual["columnas"]["estado"]]
-                )
-        except:
+            for dato in data.keys():
+            super().putDato(
+                 dato=data[dato], 
+                 fila=fila, 
+                 columna=str(dato)
+                 )
+        except Exception e:
+            print(f"ERROR METODO MODIFICA_PRODUCTO: {e}")
             return False
         else:
             return True
             
-    def proximo_retiro(self, rut: str, fecharetiro: str) -> str:
-        diascontrato = super().getDato(
-            fila=super().busca_ubicacion(
-                dato=rut,
-                columna="rut"
-                ),
-            columna="diascontrato"
-            )
-        
-        if not diascontrato:
-            return None
-        
+     def elimina_producto(self, codigo: str) -> bool:
+        fila = super().busca_ubicacion(codigo,"codigo")
+        if not fila:
+             print("ERROR METODO ELIMINA_PRODUCTO: No existe codigo indicado")
+             return False
         try:
-            int(diascontrato)
-        except Exception as e:
-            print(e)
-            return None
-        else:
-            lapso = timedelta(diascontrato + 2)
-        
-        fecharetiro = date.fromisoformat(fecharetiro)
-        proxretiro = date.isoformat(fecharetiro + lapso)
-        return proxretiro
-        
-    def guardar_modificacion(self, rut: str, data: list) -> bool:
-        fila = super().busca_ubicacion(rut,"rut")
-        try:
-            super().ingresador(fila,data,2)
-        except:
+             super().eliminar(fila)
+        except Exception e:
+            print(f"ERROR METODO MODIFICA_PRODUCTO: {e}")
             return False
         else:
             return True
+
+class SublitoteCotizacion(bdmediclean):
+     
+     def __init__(self) -> None:
+          super().__init__(
+               hoja=params.ST_COTIZACION,
+               otrolibro=params.LIBROST
+               )
+     def cotizacion_existente(self, retornoid: bool=False) -> bool:
+          datos_existentes = super().getDato(identificador="numcotizacion")
+          if datos_existentes:
+               if retornoid:
+                    return datos_existentes
+               return True
+          return False
+     
+     def idcotizacion(self) -> str:
+          nuevoid = format(datetime.now()).replace(" ","").replace(".","").replace(":","").replace("-","")
+          return 
+          
+     def nueva_cotizacion(self, retornoid: bool=False) -> bool:
+          if self.cotizacion_existente():
+               return False
+          newid = self.idcotizacion()
+          super().eliminarContenidos()
+          if super().putDato(dato=newid, identificador="numcotizacion"):
+               if retornoid:
+                    return newid
+               return True
+          return False
+     
+class SublitoteCotizacionesBD(bdmediclean):
+     
+     def __init__(self) -> None:
+          super().__init__(
+               hoja=params.ST_BD_COTIZACIONES,
+               otrolibro=params.LIBROST
+               )
+     
+     def guardar_cotizacion(self, idcotizacion: str, datos: list, modificacion: bool=False) -> bool:
+          if modificacion:
+               filalibre = super().busca_ubicacion(dato=idcotizacion,columna="idcotizacion")
+          else:
+               filalibre = super().busca_ubicacion(columna="idcotizacion")
+          try:
+               for fila in datos:
+                    fila.insert(0,idcotizacion)
+                    super().putDato(
+                         datos=fila,
+                         fila=filalibre,
+                         columna="idcotizacion"
+                         )
+                    filalibre += 1
+               else:
+                    for f in range(filalibre,self.maxfilas,1):
+                         datosremanentes = super().getDato(fila=f,columna="idcotizacion")
+                         if str(datosremanentes) == str(idcotizacion):
+                              super().eliminar(f)
+          except Exception e:
+               print(f"ERROR METODO GUARDAR_COTIZACION: Error al intentar guardar datos")
+               return False
+          else:
+               return True
+               
+     def extrae_cotizacion(self, idcotizacion: str) -> list:
+          filainicial=super().busca_ubicacion(dato=idcotizacion, columna="idcotizacion")
+          listadatos = []
+          for f in range(filainicial,self.maxfilas,1):
+               datoenfila = super().getDato(fila=f,columna="idcotizacion")
+               if str(datoenfila) != str(idcotizacion):
+                    break
+               listadatos.append(
+                    super().extraefila(
+                         fila=f,
+                         columna="todas",
+                         retornostr=True
+                         )
+                    )
+          return listadatos
+     
+     
