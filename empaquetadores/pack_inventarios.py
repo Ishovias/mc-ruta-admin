22249445a2691195511471d5a8a6cb66969e4951
IshoCelular <isaiasberoiza@gmail.com>
2024-10-08 21:56:10 -0300

    Trabajando en registro de inventarios

--- empaquetadores/pack_inventarios.py
+++ empaquetadores/pack_inventarios.py
@@ -3,8 +3,16 @@ from helpers import constructor_paquete
 
 def empaquetador_inventarios(request: object) -> map:
     paquete = constructor_paquete(request,"inventarios.html","Inventarios PDLK-25")
-    with Inventario() as inv:
-        paquete["listainventarios"] = inv.listar()
-        paquete["ultimoinventario"] = inv.getUltimoInventario()
+    def defaultPage(paquete: map) -> map:
+         with Inventario() as inv:
+             paquete["listainventarios"] = inv.listar()
+             paquete["ultimoinventario"] = inv.getUltimoInventario()
+         return paquete
     
+    paquete = defaultPage(paquete)
+    
+    if "nuevoinventario" in request.form:
+         paquete["pagina"] = "nuevoinventario.html"
+    
+     
     return paquete
