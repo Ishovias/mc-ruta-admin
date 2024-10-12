8459b846e625ebd4c8ef8402301fefa8475cfbe4
Isho-Macbook <macbook@beroro.org>
2024-10-11 22:59:53 -0300

    Trabajando en sistema de stock

--- handlers/inventarios.py
+++ handlers/inventarios.py
@@ -7,9 +7,37 @@ class Inventario(bdmediclean):
      def __init__(self) -> None:
           super().__init__(params.INVENTARIOS)
 
-     def getUltimoInventario(self) -> list:
-          datos = super().getDato(
-               fila=params.INVENTARIOS["filaStockActual"],
-               columnas=params.INVENTARIOS["columnas"]["todas"]
-          )
+     def getStockActual(self) -> map:
+          datos = {}
+          
+          columnasSeleccionadas = list(params.INVENTARIOS["columnas"].keys())
+          columnasSeleccionadas.remove(columnasSeleccionadas[0])
+          columnasSeleccionadas.remove(columnasSeleccionadas[-1])
+
+          nombresColumnas = params.INVENTARIOS["encabezados_nombre"].copy()
+          nombresColumnas.remove(nombresColumnas[0])
+          nombresColumnas.remove(nombresColumnas[0])
+          
+          for columna in columnasSeleccionadas:
+               dato = super().getDato(
+                    fila=params.INVENTARIOS["filaStockActual"],
+                    columna=columna
+               )
+               i = columnasSeleccionadas.index(columna)
+               datos[nombresColumnas[i]] = dato
+
           return datos
+
+     def modificaStock(self, elemento: str, modificacion: int) -> bool:
+          if elemento not in params.INVENTARIOS["columnas"]:
+               return False
+          filaStock = self.hoja_actual["filaStockActual"]
+          cantidadActual = super().getDato(
+               fila=filaStock,
+               columna=elemento
+          )
+          return super().putDato(
+               dato=int(cantidadActual) + modificacion,
+               fila=filaStock,
+               columna=elemento
+          )
