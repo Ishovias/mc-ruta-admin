2eba46f7c3808513178d52f674b9ffd0cb1c600b
Isho-Macbook <macbook@beroro.org>
2024-10-17 21:04:13 -0300

    Nuevo handler de registro eliminaciones

---
+++ handlers/eliminaciones.py
@@ -0,0 +1,8 @@
+from bd.repository import bdmediclean
+from params import RETIROS_ELIMINADOS
+
+class RetirosEliminados(bdmediclean):
+
+    def __init__(self):
+        super().__init__(hoja=RETIROS_ELIMINADOS)
+
