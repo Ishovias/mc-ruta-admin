1e0102e7cd7286bfcf65e86f5e8f63e9ae18d9f1
IshoCelular <isaiasberoiza@gmail.com>
2025-02-17 21:23:10 -0300

    Refactorizando 36%

--- empaquetadores/pack_inventarios.py
+++ empaquetadores/pack_inventarios.py
@@ -3,47 +3,33 @@ from helpers import constructor_paquete, cimprime
 import params
 
 def empaquetador_inventarios(request: object) -> map:
-     paquete = constructor_paquete(request,"inventarios.html","Inventarios PDLK-25")
-     def defaultPage(paquete: map) -> map:
-          with Inventario() as inv:
-               paquete["listainventarios"] = inv.listar()
-               if paquete["listainventarios"]["encabezados"][0] == None:
-                    columnasNombres = params.INVENTARIOS["encabezados_nombre"].copy()
-                    columnasNombres.remove(columnasNombres[0])
-                    paquete["listainventarios"]["encabezados"] = columnasNombres
-               paquete["stockActual"] = inv.getStockActual()
-          return paquete
+    paquete = constructor_paquete(request,"inventarios.html","Inventarios PDLK-25")
+    def datos_base():
+        with Inventario() as inv:
+            paquete["listainventarios"] = inv.listar()
+            paquete["stockActual"] = inv.mapdatos()
      
-     if "nuevoinventario" in request.form:
-          elemento = list(params.INVENTARIOS["columnas"])
-          itemsNombres = params.INVENTARIOS["encabezados_nombre"].copy()
-          
-          elemento.remove(elemento[0])
-          elemento.remove(elemento[-1])
-          itemsNombres.remove(itemsNombres[0])
-          itemsNombres.remove(itemsNombres[0])
-          
-          elementos = {}
-          for e in elemento:
-               i = elemento.index(e)
-               elementos[e] = itemsNombres[i]
-          
-          paquete["pagina"] = "nuevoinventario.html"
-          paquete["elementos"] = elementos
+    if "nuevoinventario" in request.form:
+        with Inventario() as inv:
+            elementos = inv.mapdatos()
+            del elementos["fecha"]
+            paquete["elementos"] = elementos
+        paquete["pagina"] = "nuevoinventario.html"
+
+    elif "enviarinventario" in request.form:
+        with Inventario() as inv:
+            nuevaUbicacion = inv.busca_ubicacion(columna="fecha")
+            actualizacionInv = {}
+            for columna in params.INVENTARIOS["columnas"].keys():
+                if columna == "todas":
+                    break
+                dato = request.form.get(columna)
+                inv.putDato(dato=dato,fila=nuevaUbicacion,columna=columna)
+                actualizacionInv[columna] = dato
+            if inv.actualizarStock(actualizacionInv):
+                paquete["alerta"] = "Inventario grabado y Stock actualizado"
      
-     elif "enviarinventario" in request.form:
-          with Inventario() as inv:
-               nuevaUbicacion = inv.busca_ubicacion(columna="fecha")
-               actualizacionInv = {}
-               for columna in params.INVENTARIOS["columnas"].keys():
-                    if columna == "todas":
-                         break
-                    dato = request.form.get(columna)
-                    inv.putDato(dato=dato,fila=nuevaUbicacion,columna=columna)
-                    actualizacionInv[columna] = dato
-               if inv.actualizarStock(actualizacionInv):
-                    paquete["alerta"] = "Inventario grabado y Stock actualizado"
+    else:
+        datos_base()
      
-     paquete = defaultPage(paquete)
-     
-     return paquete
+    return paquete
--- handlers/inventarios.py
+++ handlers/inventarios.py
@@ -18,46 +18,30 @@ class Inventario(bdmediclean):
                 )
         return datos
 
-    def modificaStock(self, elemento: str, modificacion: int) -> bool:
-        if elemento not in params.INVENTARIOS["columnas"]:
+    def modificaStock(self, columna: str, modificacion: int) -> bool:
+        if columna not in params.INVENTARIOS["columnas"]:
             return False
         filaStock = self.hoja_actual["filaStockActual"]
         cantidadActual = super().getDato(
             fila=filaStock,
-            columna=elemento
+            columna=columna
             )
         cantidadActual = cantidadActual if cantidadActual else 0
-        return super().putDato(
+        super().putDato(
             dato=int(cantidadActual) + modificacion,
             fila=filaStock,
-            columna=elemento
+            columna=columna
             )
+        return True
 
-     def actualizarStock(self, inventario: map) -> bool:
-          for columna, valor in inventario.items():
-               super().putDato(
-                    dato=valor,
-                    fila=params.INVENTARIOS["filaStockActual"],
-                    columna=columna
-                    )
-          else:
-               return True
-          return False
-     def obtenerInventario(self, fecha: str) -> map:
-          columnas = list(params.INVENTARIOS["columnas"].keys())
-          del(columnas["todas"])
-          datos = {}
-          ubicacion = super().busca_ubicacion(dato=fecha,columna="fecha")
-          if not ubicacion:
-               return None
-          for columna in columnas:
-               dato = super().getDato(
-                    fila=ubicacion,
-                    columna=columna
-                    )
-               if not dato:
-                    dato = 0
-               datos[columna] = dato
-          else:
-               return datos
-          return None
+    def actualizarStock(self, inventario: map) -> bool:
+        for columna, valor in inventario.items():
+            super().putDato(
+                dato=valor["dato"],
+                fila=params.INVENTARIOS["filaStockActual"],
+                columna=columna
+                )
+        else:
+            return True
+        return False
+
